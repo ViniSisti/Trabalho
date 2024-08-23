fff19d67cd592c96c791f4cabb71ad31e8187961
Guilherme D. Muller <gdm8293@gmail.com>
2024-08-23 18:28:28 -0300

    Criaçao da classe

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        return 0;
+    }
+
+    public boolean hasRepeat(ArrayList<Integer> l) {
+        return true;
+    }
+
+    public int nroRepeat(ArrayList<Integer> l) {
+        return 0;
+    }
+
+    public ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        return new ArrayList<>();
+    }
+
+    public ArrayList<Integer> union(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        return new ArrayList<>();
+    }
+
+    public ArrayList<Integer> intersect(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        return new ArrayList<>();
+    }
+}
